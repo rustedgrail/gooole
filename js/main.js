4587e6b7f8578fb897ca8b085049b002065621ba
David Newell <beta@alumni.rice.edu>
2013-02-23 14:15:11 -0500

    Stopping repeat plays when a key is held down and using the correct notes

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
 (function() {
-    keyToNote = {
-        65: 21 //A
-        , 87: 21 //W
-        , 83: 21 //S
-        , 69: 21 //E
-        , 68: 21 //D
-        , 82: 21 //R
-        , 70: 21 //F
-        , 84: 21 //T
-        , 71: 21 //G
-        , 89: 21 //Y
-        , 72: 21 //H
-        , 85: 21 //U
-        , 74: 21 //J
-        , 75: 21 //K
-    };
+    var keyToNote = { 
+        65: 24 //A, C1
+        , 87: 25 //W, C#1
+        , 83: 26 //S, D1
+        , 69: 27 //E, D#1
+        , 68: 28 //D, E1
+        , 70: 29 //F, F1
+        , 84: 30 //T, F#1
+        , 71: 31 //G, G1
+        , 89: 32 //Y, G#1 
+        , 72: 33 //H, A1
+        , 85: 34 //U, A#1 
+        , 74: 35 //J, B1 
+        , 75: 36 //K, C2 
+        };
+
+    var playing = {};
 
     document.body.addEventListener('keydown', function(e) {
-        if (!e.repeat){
+        if (!playing[e.which]) {
             MIDI.noteOn(0, keyToNote[e.which], 127, 0);
+            playing[e.which] = true;
         }
     });
 
     document.body.addEventListener('keyup', function(e) {
-            MIDI.noteOff(0, keyToNote[e.which], 0);
+        MIDI.noteOff(0, keyToNote[e.which], 0);
+        playing[e.which] = false;
     });
 
     MIDI.loadPlugin(function() {
